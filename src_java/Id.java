dcc348ddcaf8ebd1a8fa0662308627092167af3c
Andreadlc <andreadelcros7@gmail.com>
2026-02-11 11:05:16 +0100

    ajout: gestion des identifiants

---
+++ src_java/Id.java
@@ -0,0 +1,13 @@
+package pixel_tracer.src_java;
+
+public class Id {
+    private static Id instance = null;
+    private static int id = 0;
+
+    public static int get_nextId() {
+        if (instance == null) {
+            instance = new Id();
+        }
+        return id++;
+    }
+}
